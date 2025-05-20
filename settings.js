c270222749bffd194a83a6074aaa331fbda76528
RGR-CODE-OFFICIAL <rehangeeneth2@gmail.com>
2025-05-20 12:12:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'v6pUTbwC#Lffy41nDdI_CgPVDavOxb6LgduNZB6ClzrSU3rQcu50' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=A8AgxDoC#Z9uQpf-9MjfYzFDoGf7fdO2abfg5ILKgmBCZ9q1DXN0' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
